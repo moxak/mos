82be12aaf8d4be885beaa94fb6bd02cdad6d9199
moxak <smileyjoies@gmail.com>
2023-05-06 16:13:26 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // panic_handler attribute はパニックが発生したときにコンパイラが呼び出す関数
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,18 +16,7 @@ static HELLO: &[u8] = b"Hello, World!";
 
 #[no_mangle] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
-    // リンカはデフォルトで、`_start`という名前の関数を探すので
-    // この関数がエントリポイントとなる
-    
-    let vga_buffer = 0xb8000 as *mut u8;
-    
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga_buffer::print_something();
     loop {}
 }
 
