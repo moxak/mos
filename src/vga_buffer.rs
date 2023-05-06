d0c96bf2fad038a88a668e877d58857c1e16b005
moxak <smileyjoies@gmail.com>
2023-05-07 00:22:19 +0900

    update

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,7 @@
 
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -117,6 +118,14 @@ impl fmt::Write for Writer {
         self.write_string(s);
         Ok(())
     }
+}
+
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
 }
 
 pub fn print_something() {
