b2a8debe855d74c39703b1ffb48c9878bd4e4b5b
moxak <smileyjoies@gmail.com>
2023-05-07 16:44:00 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 #![no_std] // Rustの標準ライブラリにリンクしない
 #![no_main] // すべてのRustレベルのエントリポイントを無効にする
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
 
 // panic_handler attribute はパニックが発生したときにコンパイラが呼び出す関数
 #[panic_handler]
@@ -13,13 +27,35 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello, World!";
-
 #[no_mangle] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}","!");  
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[test_case]
+fn trivial_assertion() {
+    print!("tirivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
