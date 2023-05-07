00d8bb12361da7a8b02f899b0690820d608b0091
moxak <smileyjoies@gmail.com>
2023-05-07 23:26:16 +0900

    update

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(mos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
@@ -11,13 +11,16 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
-}   
-
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
+}  
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    mos::test_panic_handler(info)
+}
+
+use mos::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
